e9db80599314395bc4e874b88cea24832d1604a8
Brendan Nguyen <brendannguyen@outlook.com.au>
2026-02-05 17:56:24 +1100

    added middleware to redirect to old app for mobile users (temporary)

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const ua = req.headers.get("user-agent") || "";
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(ua);
+
+  if (isMobile) {
+    return NextResponse.redirect("https://old-brendannguyen.vercel.app");
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/"], // or ["/*"] if you want all routes
+};
